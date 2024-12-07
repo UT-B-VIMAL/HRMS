458d7aec81dd74f5492b3957cbf53debdff3e460
UT-B-Ajai <ajai.m@theunitytech.com>
2024-12-07 12:37:32 +0530

    delete route folder

--- routes/pmdashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const pmdashboardController = require('../controllers/pm/pmController');
-
-// Define routes for user-related actions (insert, update, delete)
-router.get('/', pmdashboardController.pmdashboardsection);
-
-module.exports = router;
