cf3bac802ac7ef53012ed7d719facc73c4acfead
UT-B-Ajai <ajai.m@theunitytech.com>
2024-12-24 14:23:03 +0530

    comment id

--- api/functions/subtaskFunction.js
+++ api/functions/subtaskFunction.js
@@ -159,6 +159,7 @@ exports.getSubTask = async (id, res) => {
     // Prepare comments data
     const validComments = Array.isArray(comments) && Array.isArray(comments[0]) ? comments[0] : [];
     const commentsData = validComments.map((comment) => ({
+      comments_id: comment.id || "No Comment",
       comments: comment.comments || "No Comment",
       updated_by: comment.updated_by || "Unknown User",
       shortName:comment. updated_by.substr(0, 2),
