5761e25b736a1b70fba74d571d33a562c3c17af4
SWT-LTOP-292\Vimal Sasikumar <vimal.s@theunitytech.com>
2025-01-04 15:07:02 +0530

    expense

--- api/functions/tldashboardFunction.js
+++ api/functions/tldashboardFunction.js
@@ -1084,6 +1084,7 @@ SELECT
   s.name AS subtask_name,
   s.status AS subtask_status,
   s.active_status AS subtask_active_status,
+  s.reopen_status AS subtask_reopen_status,
   s.estimated_hours AS subtask_estimation_hours,
   s.description AS subtask_description,
   te.name AS team_name,
@@ -1136,13 +1137,13 @@ AND u.deleted_at IS NULL
     const isValidSubtask = (subtask, status) => {
       switch (status) {
         case "Pending":
-          return subtask.active_status === 0 && subtask.status === 0 && subtask.reopenStatus === 0;
+          return subtask.active_status === 0 && subtask.status === 0 && subtask.reopen_status === 0;
         case "In Progress":
           return subtask.active_status === 1 && subtask.status === 1;
         case "In Review":
-          return subtask.reopenStatus === 0 && subtask.status === 2;
+          return subtask.reopen_status === 0 && subtask.status === 2;
         case "On Hold":
-          return subtask.active_status === 0 && subtask.reopenStatus === 0 && subtask.status === 1;
+          return subtask.active_status === 0 && subtask.reopen_status === 0 && subtask.status === 1;
         case "Done":
           return subtask.status === 3;
         default:
@@ -1244,6 +1245,7 @@ AND u.deleted_at IS NULL
             name: row.subtask_name,
             status: row.subtask_status,
             active_status: row.subtask_active_status,
+            reopen_status: row.subtask_reopen_status,
             estimated_hours: row.subtask_estimation_hours,
             description: row.subtask_description,
           }
