680babd6180905afc961b8adbf4dcacca8dc291f
UT-B-Kathija <kathijabeevi@theunitytech.com>
2025-01-23 11:21:50 +0530

    rating

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -1,4 +1,4 @@
-const { getAllRatings, updateRating } = require("../api/functions/ratingFunction");
+const { getAllRatings, updateRating, getRatings, getRatingById, ratingUpdation } = require("../api/functions/ratingFunction");
 const { successResponse, errorResponse } = require("../helpers/responseHelper");
 
 exports.getAllRatings = async (req, res) => {
@@ -19,5 +19,35 @@ exports.ratingUpdation = async (req, res) => {
   } catch (error) {
     const statusCode = error.status || 500;
     return errorResponse(res, error.message, "Error updating rating", statusCode);
+  }
+};
+exports.ratingUpdations = async (req, res) => {
+  try {
+    const payload = req.body;
+    await ratingUpdation(payload,res);
+  } catch (error) {
+    const statusCode = error.status || 500;
+    return errorResponse(res, error.message, "Error updating rating", statusCode);
+  }
+}
+exports.getRating = async (req, res) => {
+  try {
+    const reqbody = req.body;
+    await getRatingById(reqbody,res);
+  
+  } catch (error) {
+    const statusCode = error.status || 500;
+    return errorResponse(res, error.message, "Error fetching ratings", statusCode);
+  }
+};
+
+exports.getAllUserRating = async (req, res) => {
+  try {
+    const reqbody = req.query;
+    await getRatings(reqbody,res);
+  
+  } catch (error) {
+    const statusCode = error.status || 500;
+    return errorResponse(res, error.message, "Error fetching ratings", statusCode);
   }
 };
