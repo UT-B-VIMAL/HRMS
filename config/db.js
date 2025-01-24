03d4aa52748cb48c4c37721646459a0ae67c82ba
SWT-LTOP-292\Vimal Sasikumar <vimal.s@theunitytech.com>
2025-01-24 11:50:21 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config({ path: __dirname + '/../.env' });
 
+
 const db = mysql.createPool({
     host: process.env.DB_HOST,       
     user: process.env.DB_USERNAME,   
