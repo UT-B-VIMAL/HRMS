ed4e6c064f06f7b3b524a03c757d6f646ea026df
UT-B-VIMAL <vimal.s@theunitytech.com>
2025-05-19 10:43:54 +0530

    employee rating based on month

--- api/functions/expenseFunction.js
+++ api/functions/expenseFunction.js
@@ -1238,9 +1238,12 @@ exports.getAlltlemployeeexpense = async (req, res) => {
       [user_id]
     );
     if (teamResult.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No teams found for the given user_id" });
+       return errorResponse(
+        res,
+        null,
+        "You are not currently assigned a reporting TL for your team.",
+        404
+      );
     }
     const teamIds = teamResult.map((team) => team.id);
 
