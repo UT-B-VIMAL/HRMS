fb14aef8beb87bffa9480476b7b4366ebdf890c8
UT-B-VIMAL <vimal.s@theunitytech.com>
2025-05-27 09:53:20 +0530

    chat

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -4,14 +4,14 @@ const connectedUsers = {};
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log('Connected User for Chat:', socket.id);
+    // console.log('Connected User for Chat:', socket.id);
 
     socket.on('register', async (data) => {
       const { ticket_id, id } = data;
       const key = `${ticket_id}_${id}`;
 
       if (connectedUsers[key]) {
-        console.log(`User ${key} is already connected with socket ID ${connectedUsers[key]}. Rejecting new connection.`);
+        // console.log(`User ${key} is already connected with socket ID ${connectedUsers[key]}. Rejecting new connection.`);
         socket.emit('error', 'User is already connected on another socket.');
         socket.disconnect();
         return;
@@ -22,15 +22,15 @@ module.exports = (io) => {
           const [results] = await db.execute('SELECT id FROM users WHERE id = ?', [id]);
           if (results.length > 0) {
             connectedUsers[key] = socket.id;
-            console.log(`User ${key} registered with socket ID ${socket.id}`);
+            // console.log(`User ${key} registered with socket ID ${socket.id}`);
             socket.emit('register', `User ${key} registered with socket ID ${socket.id}`);
           } else {
-            console.log(`User ID ${id} not found.`);
+            // console.log(`User ID ${id} not found.`);
             socket.emit('error', `User ID ${id} not found.`);
           }
         } else {
           connectedUsers[key] = socket.id;
-          console.log(`User ${key} registered with socket ID ${socket.id}`);
+          // console.log(`User ${key} registered with socket ID ${socket.id}`);
           socket.emit('register', `User ${key} registered with socket ID ${socket.id}`);
         }
       } catch (err) {
@@ -49,14 +49,14 @@ module.exports = (io) => {
         );
 
         if (result.affectedRows > 0) {
-          console.log(`Updated ${result.affectedRows} record(s) in ticket_comments.`);
+          // console.log(`Updated ${result.affectedRows} record(s) in ticket_comments.`);
           socket.emit('msg', 'Message marked as read.');
         } else {
-          console.log('No records updated.');
+          // console.log('No records updated.');
           socket.emit('msg', 'No matching records found.');
         }
       } catch (error) {
-        console.error('Error updating message type:', error);
+        // console.error('Error updating message type:', error);
       }
     });
 
@@ -97,11 +97,11 @@ module.exports = (io) => {
       try {
         const { ticket_id, sender_id, receiver_id, comments } = data;
 
-        console.log('Received data:', data);
+        // console.log('Received data:', data);
 
         socket.emit('values', `ticket_id:${ticket_id}-sender_id:${sender_id}-receiver_id:${receiver_id}-comments:${comments}`);
         const istTime = moment().tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
-        console.log("isttime",istTime);
+        // console.log("isttime",istTime);
         
         
         const [result] = await db.execute(
@@ -110,7 +110,7 @@ module.exports = (io) => {
           [ticket_id, sender_id, receiver_id, comments, istTime, istTime]
         );
 
-        console.log(`Message inserted into ticket_comments with ID: ${result.insertId}`);
+        // console.log(`Message inserted into ticket_comments with ID: ${result.insertId}`);
         socket.emit('datas', result.insertId);
 
         const [resultData] = await db.execute(
@@ -140,7 +140,7 @@ module.exports = (io) => {
 
         const recipientSocketId = connectedUsers[key];
         if (recipientSocketId) {
-          console.log("socketmsgdata",{ ...resultData[0] });
+          // console.log("socketmsgdata",{ ...resultData[0] });
           
           io.to(recipientSocketId).emit('chat message', { ...resultData[0] });
           socket.emit('msg', 'Msg sended.');
@@ -151,7 +151,7 @@ module.exports = (io) => {
     });
 
     socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
+      // console.log('User disconnected:', socket.id);
 
       let disconnectedUser = null;
       Object.keys(connectedUsers).forEach((key) => {
